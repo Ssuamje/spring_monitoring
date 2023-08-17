6e5799c11eedb500b336d76cfe3d11ba23ebf971
Ssuamje <side2850@naver.com>
2023-08-17 21:26:08 +0900

    Start Project

---
+++ src/test/java/korean/prometheus/grafana/GrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package korean.prometheus.grafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
